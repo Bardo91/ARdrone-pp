bbf0ba91b9741b11d9687ad159ca1bd02c550dba
Bardo91 <19Bardo91@gmail.com>
2015-04-06 20:55:43 +0200

    Added ARdrone wrapper class that holds ARdrone behavior.

---
+++ src/Ardrone.h
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//		ARdrone++: library
+//			Author: Pablo R.S. (A.k.a. Bardo91)
+//			Date:	2015-APR-06
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Main Ardrone class
+
+#ifndef ARDRONEPP_ARDRONE_H_
+
+#include "core/comm/UdpSocket.h"
+
+namespace ardronepp{
+
+	class Ardrone{
+	public:
+		Ardrone();
+
+		void takeOff();
+		void land();
+	private:
+		UdpSocket mControlSocket;
+	};	//	class Ardrone
+
+}	//	namespace ardronepp
+
+#endif // ARDRONEPP_ARDRONE_H_
+
--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,19 @@
 // Main to test library
 
 
-#include "core/comm/UdpSocket.h"
+#include "Ardrone.h"
 #include "core/time/Time.h"
 
 int main(int _argc, char **_argv){
 	_argc;
 	_argv;
 
-	ardronepp::UdpSocket control("192.168.1.1", 5556);
-
-	control.send("AT*REF=101,290718208\r");
-
+	ardronepp::Ardrone drone;
+	
+	drone.takeOff();
 	ardronepp::STime::get()->delay(1);
-
-	control.send("AT*REF=102,290717696\r");
-
-
-
+	drone.land();
+	
 	system("PAUSE");
 
 }
