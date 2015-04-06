69ea6a9e337a4b18e95cda2da212f05e6e2d90c5
Bardo91 <19Bardo91@gmail.com>
2015-04-06 20:14:59 +0200

    Added STime class as time tool

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 
 #include "core/comm/UdpSocket.h"
+#include "core/time/Time.h"
 
 int main(int _argc, char **_argv){
 	_argc;
@@ -16,8 +17,10 @@ int main(int _argc, char **_argv){
 
 	ardronepp::UdpSocket control("192.168.1.1", 5556);
 
-
 	control.send("AT*REF=101,290718208\r");
+
+	ardronepp::STime::get()->delay(1);
+
 	control.send("AT*REF=102,290717696\r");
 
 
