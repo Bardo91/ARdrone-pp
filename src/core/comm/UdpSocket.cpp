c4c1fae4b686968099584b8e918b2e1876fb2eeb
Bardo91 <19Bardo91@gmail.com>
2015-04-08 20:44:57 +0200

    Set sendto socket in udp socket implementation as nonblocking operation

--- src/core/comm/UdpSocket.cpp
+++ src/core/comm/UdpSocket.cpp
@@ -58,7 +58,14 @@ namespace ardronepp{
 	int UdpSocket::send(const std::string &_msg){
 		assert(mSocket != INVALID_SOCKET);
 
-		int n = sendto(mSocket, _msg.c_str(), _msg.size(), 0, (sockaddr*)&mDroneAddr, sizeof(mDroneAddr));
+		int flags = 0;
+		#if defined(_WIN32)
+			flags |= WSAEWOULDBLOCK;
+		#elif defined(__linux__)
+			flags |= MSG_DONTWAIT;
+		#endif
+
+		int n = sendto(mSocket, _msg.c_str(), _msg.size(), flags, (sockaddr*)&mDroneAddr, sizeof(mDroneAddr));
 		
 
 		return n < 1 ? 0 : n;
