853c111eaa550c38248f80592971fbba078edf79
Bardo91 <19Bardo91@gmail.com>
2015-04-10 20:13:19 +0200

    Added inline doc in Ardrone commands

--- src/drone/Ardrone.h
+++ src/drone/Ardrone.h
@@ -23,9 +23,10 @@ namespace ardronepp{
 		void takeOff();
 		void land();
 		void hovering();
-		void spin(float _yawSpeed);
-		void lift(float _vSpeed);
-		void translate(float _pitch, float _roll);
+		void spin(float _yawSpeed);					// _yawSpeed	= [-1, 1]	left - right
+		void lift(float _vSpeed);					// _vSpeed		= [-1, 1]	ip - down
+		void translate(float _pitch, float _roll);	// _pitch		= [-1, 1]	front - back
+													// _roll		= [-1, 1]	left - right
 
 	private:
 		UdpSocket mControlSocket;
